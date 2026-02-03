5539b98ed626fb44f0cae66d5fb7f242ada01f6e
Kim Myeong Jin <myeonggoo1026@gmail.com>
2026-02-03 16:12:53 +0900

    Delete src directory

--- src/main/java/com/example/mymusic_server/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mymusic_server;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Integer> {
-    // 데이터 조회 기능
-}
